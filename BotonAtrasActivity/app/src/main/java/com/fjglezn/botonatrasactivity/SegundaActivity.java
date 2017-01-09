c0f65958bd19c5a57c2f6ed51c8cc0ab0989984d
fjGlezn <fjglezn@outlook.com>
2017-01-08 23:05:58 -0600

    Boton Atrás (Subir)
    Implementación del botón de regreso en el actionbar.

---
+++ BotonAtrasActivity/app/src/main/java/com/fjglezn/botonatrasactivity/SegundaActivity.java
@@ -0,0 +1,15 @@
+package com.fjglezn.botonatrasactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
